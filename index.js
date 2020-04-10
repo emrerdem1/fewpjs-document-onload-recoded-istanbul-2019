0908fcd0e6f9151bbd5df9803814dead0fb3a75d
emrerdem1 <emrerdem94@gmail.com>
2020-04-10 23:15:16 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    changep();
+});
+
+
+function changep() {
+    document.getElementById("text").innerHTML = "This is really cool!";
+};
